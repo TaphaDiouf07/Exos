96faf633fdfcb7561843b57f1260df75ad234fbf
TaphaDiouf07 <taphadiouf5@hotmail.com>
2026-02-03 21:55:41 +0000

    Réalisation programme entiers_impairs

--- Exo.c
+++ Exo.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 int main(){
     int i,n,nombre;
-    int sommePairs = 0;
-    int compteurPairs = 0;
+    int sommeImpairs = 0;
+    int compteurImpairs = 0;
     float moyenne;
 
     printf("Combien de nombres voulez-vous entrer ? ");
     scanf("%d",&n);
 
-    for(i = 1; i <= n; i++){
+    for(i = 1; i <= n; i+=1){
         printf("Entrez le nombre numero %d : ", i);
         scanf("%d", &nombre);
-        if(nombre % 2 == 0){
-            sommePairs += nombre;
-            compteurPairs += 1;
+        if(nombre % 2 != 0){
+            sommeImpairs += nombre;
+            compteurImpairs += 1;
         }
     }
-        if (compteurPairs > 0){
-            moyenne = (sommePairs * 1.0) / compteurPairs;
-            printf("\nLa moyenne des %d nombres pairs est : %.2f\n", compteurPairs, moyenne);
+        if (compteurImpairs > 0){
+            moyenne = (sommeImpairs * 1.0) / compteurImpairs;
+            printf("\nLa moyenne des %d nombres pairs est : %.2f\n", compteurImpairs, moyenne);
         } else {
-            printf("\nVous n'avez saisi aucun nombre pair.\n");
+            printf("\nVous n'avez saisi aucun nombre impair.\n");
         }
     return 0;
 }
